dcef9de86a448b9cb54b7fd658a977c4535af17d
Abhinav <143058543+Abhyxz@users.noreply.github.com>
2025-02-19 08:21:25 -0500

    Assignment 1
    Assignment 1

---
+++ assignment 1/src/client/app/app.js
@@ -0,0 +1,7 @@
+// Name: Abhinav
+// Course: INFT 2202
+// Date: Today's Date
+// Description: Common functions for all pages.
+
+// Set the current year in the footer
+document.getElementById('copyrightYear').textContent = new Date().getFullYear();
